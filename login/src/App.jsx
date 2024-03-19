2efea7be588c53305efd188cb469125e5f77af60
VedantAnand17 <vedantanand0077@gmail.com>
2024-03-19 23:58:19 +0530

    updated code

---
+++ login/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from './components/login.jsx'
+
+function App() {
+  return (
+    <div className="w-full h-screen text-black">
+      <Login />
+    </div>
+  )
+}
+
+export default App
